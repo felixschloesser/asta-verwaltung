8a4b24fc35aa500e5c39b55b0759a08d54c847c3
Felix Schlösser <fs@TinTinBook.fritz.box>
2020-06-20 22:48:24 +0200

    added generic views

--- polls/urls.py
+++ polls/urls.py
@@ -6,15 +6,8 @@ from . import views
 app_name = 'polls' # Namespace: polls:detail, polls:results
 
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-
-    # ex: /polls/6/results
-    path('<int:question_id>/results/', views.results, name='results'),
-
-    # ex: /polls/2/vote
-    path('<int:question_id>/vote', views.vote, name='vote')
+    path('', views.IndexView.as_view(), name='index'), # ex: /polls/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'), # ex: /polls/5/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'), # ex: /polls/6/results
+    path('<int:question_id>/vote', views.vote, name='vote') # ex: /polls/2/vote
 ]

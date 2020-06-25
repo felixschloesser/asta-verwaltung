60e0abe4992d5b2113f34986e4d32ecc0324c27a
felixschloesser <felix.schloesser@tuhh.de>
2020-06-25 19:59:31 +0200

    added buildings

--- keys/admin.py
+++ keys/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Building, Room, Door, Key, LockingSystem
+
+class RoomInline(admin.TabularInline):
+    model = Room
+    extra = 1
+
+
+class BuildingAdmin(admin.ModelAdmin):
+    inlines = [RoomInline]
+
+
+class KeyAdmin(admin.ModelAdmin):
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "doors":
+            kwargs["queryset"] = Car.objects.filter(owner=request.user)
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
+
+
+class DoorAdmin(admin.ModelAdmin):
+    # List
+    list_display = ('__str__', 'gebäude', 'comment', 'active')
+    #list_filter = ['gebäude', 'active', 'comment']
+
+admin.site.register(Building, BuildingAdmin)
+admin.site.register(Key, KeyAdmin)
+admin.site.register(Door, DoorAdmin)
+admin.site.register(LockingSystem)
--- keys/models.py
+++ keys/models.py
@@ -2,37 +2,67 @@ from django.db import models
 
 # Create your models here.
 class Building(models.Model):
-    identifier = models.CharField("Identifier", max_length=8, unique=True) # not NULL
+    identifier = models.CharField(max_length=8, unique=True) # not NULL
+
+    def __str__(self):
+        return self.identifier
 
 
 class Room(models.Model):
-    number = models.CharField("Room number",max_length=32)
-    building = models.ForeignKey('Building', on_delete = models.CASCADE)
+    name = models.CharField(max_length=32, unique=True, blank=True)
+    number = models.CharField("room number",max_length=32)
+    building = models.ForeignKey('building', on_delete = models.CASCADE)
+    purpose_choices = [('committee', 'Gremienraum'),
+               ('storage', 'Lager'),
+               ('learning', 'Lernraum'),
+               ('multipurpose', 'Mehrzweckraum'),
+               ('other', 'Other')]
+    purpose = models.CharField(max_length=32, choices=purpose_choices)
 
     class Meta:
-        models.UniqueConstraint(fields=['building', 'number'], name='room_is_unique')
+        constraints = [
+            models.UniqueConstraint(fields=['building', 'number'], name='room_is_unique')
+        ]
+
+    def __str__(self):
+        if self.name:
+            return self.name
+        else:
+            return "{}{}".format(self.building,self.number)
 
 
 class Door(models.Model):
-    room = models.ForeignKey('Room', on_delete = models.CASCADE)
+    room = models.ForeignKey("room", on_delete=models.PROTECT)
+    active = models.BooleanField("Aktiv", default=True)
     comment = models.CharField(max_length=64, blank=True)
 
+    def __str__(self):
+        if self.comment:
+            return "zum {} ({})".format(self.room, self.comment)
+        else:
+            return "zum {}".format(self.room)
+
+    def gebäude(self):
+        return self.room.building
 
 class Key(models.Model):
-    number = models.CharField("Key number", max_length=32)
-    doors = models.ManyToManyField("Door")
-    locking_system = models.ForeignKey('LockingSystem', on_delete = models.CASCADE)
-# if not rentent -> require location, key-safe?
+    number = models.CharField("key number", max_length=32)
+    doors = models.ManyToManyField("door")
+    locking_system = models.ForeignKey('lockingSystem', on_delete = models.CASCADE)
+# if not rented -> require location, key-safe?
+
 
 class LockingSystem(models.Model):
-    name = models.CharField("Name", max_length=32, unique=True)
-    kind = [('mechanical', 'Mechanical'),
-            ('mechatronical', 'Mechatronical'),
-            ('transponder', 'Transponder')]
+    name = models.CharField(max_length=32, unique=True)
+    kind = [('mechanical', 'mechanical key'),
+            ('mechatronical', 'mechatronical key'),
+            ('transponder', 'transponder')]
     company = models.CharField(max_length=32, unique=True)
-
     class Meta:
-        models.UniqueConstraint(fields=['name', 'company'], name='locking_system_is_unique')
+        constraints = [
+            models.UniqueConstraint(fields=['name', 'company'], name='locking_system_is_unique')
+        ]
+
 # Persons
 #EmailField
 

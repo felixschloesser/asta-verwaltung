14213f3b2f5475328fd03b86d48e76742ee6e791
felixschloesser <felix.schloesser@tuhh.de>
2021-02-23 23:12:41 +0100

    fixed path spelling for custom OpenID backend

--- asta-administration/settings.py
+++ asta-administration/settings.py
@@ -161,7 +161,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 
-AUTHENTICATION_BACKENDS = ['asta-administration.auth.CustomOidBackend']
+AUTHENTICATION_BACKENDS = ['asta-administration.auth.CustomOpenidBackend']
 
 OIDC_RP_CLIENT_ID = os.getenv('OIDC_RP_CLIENT_ID')
 OIDC_RP_CLIENT_SECRET = os.getenv('OIDC_RP_CLIENT_SECRET')

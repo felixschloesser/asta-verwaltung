00f51f2a493c0793715d029fbe34c8870f451366
Felix Schlösser / TinTin <dev@felixschloesser.de>
2021-02-24 19:50:33 +0100

    implemented proper verify claims and create_user

--- asta-administration/auth.py
+++ asta-administration/auth.py
@@ -9,16 +9,35 @@ class CustomOpenidBackend(OIDCAuthenticationBackend):
         logging.debug(claims)
         logging.debug(claims.get('read_user'))
         logging.debug("reading_claims")
-        is_admin = 'AStA/Mitglieder/Vorstand' in claims.get('group', [])
-        logging.debug(is_admin)
-        return verified and True
+
+        claimed_groups = claims.get('groups', [])
+
+        authorized_groups = [
+            'asta/mitarbeitende',
+            'asta/mitglieder/vorstand',
+            'asta/mitglieder/it',
+        ]
+
+        # Return True if any of the groups in the authorized groups list is found in the claimed groups
+        authorized = any(item in claimed_groups for item in authorized_groups)
+
+        return verified and authorized
+
+
+    def get_username(self, claims):
+        nickname = claims.get('nickname')
+        return nickname
+
 
     def create_user(self, claims):
         """Return object for a newly created user account."""
         email = claims.get('email')
+        name = claims.get('name')
+        fist_name = name.pop(0)
+        last_name = name.pop()
 
         logging.info("Creating debug")
         logging.info(claims)
 
         username = self.get_username(claims)
-        return self.UserModel.objects.create_user(username, email)
+        return self.UserModel.objects.create_user(username, email, first_name=fist_name, last_name=last_name)

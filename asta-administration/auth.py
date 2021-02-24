bf8d9c4bc7ddaccb19d1ac1be544e46be51cbd41
Felix Schlösser / TinTin <dev@felixschloesser.de>
2021-02-24 12:52:05 +0100

    modified openid scope

--- asta-administration/auth.py
+++ asta-administration/auth.py
@@ -5,13 +5,15 @@ import logging
 class CustomOpenidBackend(OIDCAuthenticationBackend):
     def verify_claims(self, claims):
         verified = super().verify_claims(claims)
-        is_admin = 'vorstand' in claims.get('group', [])
+        logging.debug(claims.get('group', []))
+        print(claims.get('group', []))
+        is_admin = 'AStA/Mitglieder/Vorstand' in claims.get('group', [])
         return verified and is_admin
 
     def create_user(self, claims):
         """Return object for a newly created user account."""
         email = claims.get('email')
-        
+
         logging.info("Creating debug")
         logging.info(claims)
 
